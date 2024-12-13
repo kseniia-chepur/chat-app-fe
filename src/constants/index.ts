4f0e3c0d76bbe56c00b9c882f2f0697a95f6c905
Kseniia Chepur <kseniia.chepur@gmail.com>
2024-12-13 16:59:16 +0200

    update backend url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const API_URL = {
   cloudinary: 'https://api.cloudinary.com/v1_1/dzudu6utt/auto/upload',
-  backend: 'http://localhost:8000',
+  backend: 'https://chat-app-1mkx.onrender.com',
 };
 
 export const bgColors = [
